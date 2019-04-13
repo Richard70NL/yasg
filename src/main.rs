45723205c5fa49fcd138c6e9c7d6178e1e002288
Richard70NL <richard@richard70.nl>
2019-04-13 02:11:19 +0200

    initial command line arguments processing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+/************************************************************************************************/
+
+#[macro_use]
+extern crate clap;
+
+/************************************************************************************************/
+
+use clap::SubCommand;
+use std::io;
+
+/************************************************************************************************/
+
 fn main() {
-    println!("yasg says: Hello, world!");
+    let mut app =
+        app_from_crate!().subcommand(SubCommand::with_name("build").about("builds the site"));
+    let matches = app.clone().get_matches();
+
+    match matches.subcommand {
+        None => {
+            let mut out = io::stdout();
+            app.write_long_help(&mut out)
+                .expect("failed to write to stdout");
+            println!();
+        }
+        Some(cmd) => {
+            if cmd.name == "build" {
+                perform_build();
+            }
+        }
+    }
 }
+
+/************************************************************************************************/
+
+fn perform_build() {
+    println!("BUILDING...");
+}
+
+/************************************************************************************************/
